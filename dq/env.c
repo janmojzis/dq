2b665e308333fb5b1aa93bd7d782949e2a2731b7
apache <jan.mojzis@firma.seznam.cz>
2016-01-22 09:41:12 +0100

    update env lib

--- dq/env.c
+++ dq/env.c
@@ -1,27 +1,18 @@
-/*
-20130505
-Jan Mojzis
-Public domain.
-*/
-
+#include "str.h"
 #include "env.h"
 
-static char *findkey(const char *y, const char *x) {
+/*
+The 'env_get(name)' function returns the value of the first environment
+variable whose name is 'name', or 0 if there is no such variable.
+*/
+char *env_get(const char *name) {
 
-    while(*x && *y) if (*y++ != *x++) return (char *)0;
-    if (!*x && *y == '=') return (char *)(y + 1);
-    return (char *)0;
-}
+    long long len, i;
 
-char *env_get(const char *s) {
-
-    char *r;
-    long long i;
-
-    if (!s) return (char *)0;
-    for (i = 0; environ[i]; ++i) {
-        r = findkey(environ[i], s);
-        if (r) return r;
-    }
-    return (char *)0;
+    if (!name) return 0;
+    len = str_len(name);
+    for (i = 0; environ[i]; ++i)
+        if (str_start(environ[i], name) && (environ[i][len] == '='))
+            return environ[i] + len + 1;
+    return 0;
 }
