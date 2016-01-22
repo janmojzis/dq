3e71c7ccd5969eddd26cf5bf774aa5308164ea63
apache <jan.mojzis@firma.seznam.cz>
2016-01-22 09:53:48 +0100

    debian - systemd support

---
+++ debian/dqcache-wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+. /etc/default/dqcache
+export ROOT
+export CACHESIZE
+export IP
+
+if [ x"${ROOT}" = x ]; then
+  echo 'dqcache-extreme.sh: $ROOT not set' >&2
+  exit 111
+fi
+
+ID=`expr "$$" + "141500000"`
+chown "${ID}.${ID}" "${ROOT}/dump"
+chown "${ID}.${ID}" "${ROOT}/dump/dnsdata" 
+exec /usr/bin/env UID="${ID}" GID="${ID}" /usr/sbin/dqcache
