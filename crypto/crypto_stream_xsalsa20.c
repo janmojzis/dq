8d0cdae68c1f81ac6cfc0f996571fef14e63c0eb
Jan Mojzis <github@mojzis.com>
2016-03-24 14:22:27 +0100

    prevent compiler to remove cleanup code

--- crypto/crypto_stream_xsalsa20.c
+++ crypto/crypto_stream_xsalsa20.c
@@ -14,7 +14,6 @@ static const unsigned char sigma[16] = "expand 32-byte k";
 int crypto_stream_xsalsa20_tinynacl_xor(unsigned char *c, const unsigned char *m, unsigned long long l, const unsigned char *n, const unsigned char *k) {
 
     unsigned char subkey[32];
-    long long i;
 
     crypto_core_hsalsa20(subkey, n, k, sigma);
     crypto_stream_salsa20_xor(c, m, l, n + 16, subkey);
