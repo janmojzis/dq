405016c293e899faaa0a7aa670b934b5fd60199d
Jan Mojzis <jan.mojzis@gmail.com>
2018-01-04 21:53:59 +0100

    removed little/big endian detection/optization

--- crypto/uint64_pack.c
+++ crypto/uint64_pack.c
@@ -1,4 +1,3 @@
-#include "order.h"
 #include "uint64_pack.h"
 
 /*
@@ -7,10 +6,6 @@ integer into 8 bytes stored in little-endian format
 */
 void uint64_pack(unsigned char *y, crypto_uint64 x) {
 
-#ifdef ORDER_LITTLEENDIAN
-    *(crypto_uint64 *)y = x;
-#else
     long long i;
     for (i = 0; i < 8; ++i) { y[i] = x; x >>= 8; }
-#endif
 }
--- crypto/uint64_unpack.c
+++ crypto/uint64_unpack.c
@@ -1,4 +1,3 @@
-#include "order.h"
 #include "uint64_unpack.h"
 
 /*
@@ -7,12 +6,8 @@ in little-endian format into 64-bit unsigned integer.
 */
 crypto_uint64 uint64_unpack(const unsigned char *x) {
 
-#ifdef ORDER_LITTLEENDIAN
-    return *(crypto_uint64 *)x;
-#else
     crypto_uint64 y = 0;
     long long i;
     for (i = 7; i >= 0; --i) y = (y << 8) | x[i];
     return y;
-#endif
 }
