e6c5a448ce359432581207a70a7bf14b9fd5b8e3
Jan Mojžíš <jan.mojzis@gmail.com>
2024-10-27 19:29:19 +0100

    roots.h: fix mismatched bound [-Warray-parameter=]

--- roots.h
+++ roots.h
@@ -1,7 +1,7 @@
 #ifndef ROOTS_H
 #define ROOTS_H
 
-extern int roots(unsigned char *, unsigned char *, int *, unsigned char *);
+extern int roots(unsigned char [256], unsigned char [528], int *, unsigned char *);
 extern int roots_same(unsigned char *, unsigned char *);
 extern int roots_init(char *);
 
